32c335da5cba9d73e0be48977e2cbf6a2ead16aa
Vladyslav Kolomiets <vladyslavkolomiets@gmail.com>
2019-04-01 17:58:32 +0300

    created UI

--- Rock-Paper-Scissors/Rock-Paper-Scissors/HomeVC.swift
+++ Rock-Paper-Scissors/Rock-Paper-Scissors/HomeVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeVC.swift
 //  Rock-Paper-Scissors
 //
 //  Created by Vladyslav Kolomiets on 4/1/19.
@@ -8,13 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeVC: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    self.view.backgroundColor = #colorLiteral(red: 0.2470338643, green: 0.2470747828, blue: 0.2470248342, alpha: 1)
   }
-
-
+  
+  
+  
 }
 
---
+++ Rock-Paper-Scissors/Rock-Paper-Scissors/Sign.swift
@@ -0,0 +1,12 @@
+//
+//  Sign.swift
+//  Rock-Paper-Scissors
+//
+//  Created by Vladyslav Kolomiets on 4/1/19.
+//  Copyright © 2019 Vladyslav Kolomiets. All rights reserved.
+//
+
+import Foundation
+import GameplayKit
+
+
