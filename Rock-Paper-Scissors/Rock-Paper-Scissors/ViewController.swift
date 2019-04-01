ba5e3407fe39b20c80d9066a9d9329bbdd626fa3
Vladyslav Kolomiets <vladyslavkolomiets@gmail.com>
2019-04-01 17:24:49 +0300

    create project

---
+++ Rock-Paper-Scissors/Rock-Paper-Scissors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rock-Paper-Scissors
+//
+//  Created by Vladyslav Kolomiets on 4/1/19.
+//  Copyright © 2019 Vladyslav Kolomiets. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
